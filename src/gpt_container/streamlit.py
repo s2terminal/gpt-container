3ebe8527c2ec6a453d7ef9f3dde190abb99494e0
s2terminal <suzuki.sh@s2terminal.com>
2024-09-07 12:57:23 +0900

    get cuda device name

--- src/gpt_container/streamlit.py
+++ src/gpt_container/streamlit.py
@@ -1,8 +1,20 @@
 import streamlit
 from generator import Generator
+import torch
 
 generator_cpu = Generator()
-generator_cuda = Generator(device="cuda:0")
+generator_cuda = None
+
+with streamlit.expander("cuda device"):
+    if torch.cuda.is_available():
+        streamlit.text(f'cuda.get_device_name: {torch.cuda.get_device_name()}')
+        streamlit.text(f'cuda.get_device_capability: {torch.cuda.get_device_capability()}')
+        streamlit.text(f'cuda.device_count: {torch.cuda.device_count()}')
+        streamlit.text(f'cuda.current_device: {torch.cuda.current_device()}')
+        cuda_device = f'cuda:{torch.cuda.current_device()}'
+        generator_cuda = Generator(device=cuda_device)
+    else:
+        streamlit.text('no cuda device')
 
 input_text = streamlit.text_input("入力してね", value="")
 
@@ -14,7 +26,8 @@ if len(input_text) > 0:
     streamlit.markdown(result["output"])
     streamlit.text(f'実行時間: {result["time"]:.3f}秒')
 
-    streamlit.text('GPU:')
-    result = generator_cuda.generate(input_text)
-    streamlit.markdown(result["output"])
-    streamlit.text(f'実行時間: {result["time"]:.3f}秒')
+    if generator_cuda is not None:
+        streamlit.text('GPU:')
+        result = generator_cuda.generate(input_text)
+        streamlit.markdown(result["output"])
+        streamlit.text(f'実行時間: {result["time"]:.3f}秒')
